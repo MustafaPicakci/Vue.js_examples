2d71fb5eca7eda5060e7158bad5a99a4a48a66fb
Mustafa Pıçakçı <mustafapicakci25@gmail.com>
2020-10-24 01:26:28 +0300

    monster game class binding

--- 3-Simple-Monster-Game/app.js
+++ 3-Simple-Monster-Game/app.js
@@ -3,7 +3,8 @@ new Vue({
     data:{
         player_heal: 100,
         monster_heal: 100,
-        game_is_on: false
+        game_is_on: false,
+        logs:[]
     },
     methods:{
         start_game(){
@@ -11,25 +12,33 @@ new Vue({
         },
         attack(){
             var point =Math.ceil(Math.random() * 10);
+            this.add_to_log({turn:"p", text:"OYUNCU ATAĞI ("+ point + " )"})
             this.monster_heal -=point;
             this.monster_attack();
         },
         special_attack(){
             var point =Math.ceil(Math.random() * 25);
+            this.add_to_log({turn:"p", text:"ÖZEL OYUNCU ATAĞI ("+ point + " )"})
             this.monster_heal -=point;
             this.monster_attack();
         },
         monster_attack(){
             var point =Math.ceil(Math.random() * 15);
+            this.add_to_log({turn:"m", text:"CANAVAR ATAĞI ("+ point + " )"})
             this.player_heal -=point;
         },
         heal_up(){
             var point =Math.ceil(Math.random() * 20);
+            this.add_to_log({turn:"p", text:"ILK YARDIM ("+ point + " )"})
             this.player_heal +=point;
             this.monster_attack();
         },
         give_up(){
             this.player_heal=0;
+            this.add_to_log({turn:"p", text:"OYUNCU PES ETTİ"})
+        },
+        add_to_log(log){
+            this.logs.push(log);
         }
     },
     watch:{
@@ -40,6 +49,7 @@ new Vue({
                 if(confirm("Kaybettin... Tekrar deneme istser misin ?")){
                     this.player_heal = 100;
                     this.monster_heal = 100;
+                    this.logs=[];
                 }
             }else if(value>=100){
                 this.player_heal=100;
@@ -48,9 +58,10 @@ new Vue({
         monster_heal(value){
             if(value <= 0){
                 this.monster_heal = 0;
-                if(confirm("Kaybettin... Tekrar deneme istser misin ?")){
+                if(confirm("Kazandın... Tekrar deneme istser misin ?")){
                     this.player_heal = 100;
                     this.monster_heal = 100;
+                    this.logs=[];
                 }
             }
         }
