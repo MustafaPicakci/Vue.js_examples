a0b946c3ecb03f2eac25e2fb884adb78a003f0ca
Mustafa Pıçakçı <mustafapicakci25@gmail.com>
2020-10-24 00:45:34 +0300

    monster-game ortamın hazırlanması

---
+++ 3-Simple-Monster-Game/app.js
@@ -0,0 +1,25 @@
+new Vue({
+    el:"#app",
+    data:{
+        player_heal: 100,
+        monster_heal: 100,
+        game_is_on: false
+    },
+    methods:{
+        start_game(){
+            this.game_is_on=true;
+        },
+        attack(){
+
+        },
+        special_attack(){
+
+        },
+        heal_up(){
+
+        },
+        give_up(){
+
+        }
+    }
+})
