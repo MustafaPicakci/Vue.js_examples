26cad6c2c7420d717210b2784d36caa803618869
Mustafa Pıçakçı <mustafapicakci25@gmail.com>
2020-10-27 16:54:39 +0300

    slot ile içerik göndermek

---
+++ 8-vue-cli-gelismis-component-kullanimi/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6 col-md-offset-3">
+        <appMemory>
+          <h1>Güzel bir gün...</h1>
+          <p>bu çok güzel bir anıydı.</p>
+        </appMemory>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Memory from "./Memory";
+
+export default {
+  components: {
+    appMemory: Memory
+  }
+};
+</script>
---
+++ 8-vue-cli-gelismis-component-kullanimi/src/Memory.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+     <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    props:["content"]
+}
+</script>
+
+<style scoped> 
+    div{
+        border:1px solid #ccc;
+        box-shadow: 1px 1px 2px #666;
+        padding: 20px 20px;
+        margin-bottom: 20px;
+    }
+</style>
