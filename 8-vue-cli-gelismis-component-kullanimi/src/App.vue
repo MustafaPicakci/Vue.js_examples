6f44de99d004680f9059132b5e404a177615644c
Mustafa Pıçakçı <mustafapicakci25@gmail.com>
2020-10-27 23:45:51 +0300

    Birden fazla component arasında dinamik olarak switch işlemi

--- 8-vue-cli-gelismis-component-kullanimi/src/App.vue
+++ 8-vue-cli-gelismis-component-kullanimi/src/App.vue
@@ -2,21 +2,59 @@
   <div class="container">
     <div class="row">
       <div class="col-md-6 mx-auto">
-        <appMemory>
+        <br /><br />
+        <button
+          @click="selectedComponent = 'appMemory'"
+          class="btn-sm btn btn-primary"
+        >
+          appMemory
+        </button>
+        <button
+          @click="selectedComponent = 'appNewMemory'"
+          class="btn-sm btn btn-primary"
+        >
+          appNewMemory
+        </button>
+        <button
+          @click="selectedComponent = 'appAuthor'"
+          class="btn-sm btn btn-primary"
+        >
+          appAuthor
+        </button>
+        <hr />
+        <p>{{ selectedComponent }}</p>
+        <hr />
+        <br />
+
+        <component :is="selectedComponent">
           <h1 slot="heading">Güzel bir gün...</h1>
           <p>bu çok güzel bir anıydı.</p>
-        </appMemory>
+        </component>
+
+        <!-- <appMemory>
+          <h1 slot="heading">Güzel bir gün...</h1>
+          <p>bu çok güzel bir anıydı.</p>
+        </appMemory> !-->
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import Memory from "./Memory";
+import Memory from "./components/Memory";
+import NewMemory from "./components/NewMemory";
+import Author from "./components/Author";
 
 export default {
+  data() {
+    return {
+      selectedComponent: "appMemory"
+    };
+  },
   components: {
-    appMemory: Memory
+    appMemory: Memory,
+    appNewMemory: NewMemory,
+    appAuthor: Author
   }
 };
 </script>
