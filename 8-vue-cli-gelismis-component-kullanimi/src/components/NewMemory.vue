87033fe6af8ecad902ee4e26bc4ae232933adca8
Mustafa Pıçakçı <mustafapicakci25@gmail.com>
2020-10-27 23:56:14 +0300

    Dinamik Component'lerde LifeCycle Hook(activated,deactivated)

--- 8-vue-cli-gelismis-component-kullanimi/src/components/NewMemory.vue
+++ 8-vue-cli-gelismis-component-kullanimi/src/components/NewMemory.vue
@@ -1,12 +1,21 @@
 <template>
   <div>
-   <h1>newMemory componenti</h1>
+    <h1>newMemory componenti</h1>
   </div>
 </template>
 
 <script>
+//keep-alive kullandığımız için destroyed hook'u artık kullanılamıyor fakat 2 yeni hood eklenmiş oluyor. hangi komponenting aktif
+//olduğunu activated ve deavtivated ile tespit edebiliyoruz
 export default {
- 
-}
+  destroyed() {
+    console.log("destroyed");
+  },
+  activated() {
+    console.log("activated");
+  },
+  deactivated() {
+    console.log("deactivated");
+  }
+};
 </script>
-
