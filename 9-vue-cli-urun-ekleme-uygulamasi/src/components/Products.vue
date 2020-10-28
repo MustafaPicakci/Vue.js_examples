357fef0dbcd3875101c3e70a547625dbb0c8fd66
Mustafa Pıçakçı <mustafapicakci25@gmail.com>
2020-10-28 17:49:40 +0300

    urun ekleme uygulaması - Ürün listesinin dinamik olarak getirilmesi

--- 9-vue-cli-urun-ekleme-uygulamasi/src/components/Products.vue
+++ 9-vue-cli-urun-ekleme-uygulamasi/src/components/Products.vue
@@ -3,16 +3,16 @@
     <appProduct v-for="product in productList" :key="product">
       <img
         class="card-img-top"
-        src="/src/assets/default.png"
-        alt="Card image cap"
+        :src="product.selectedImage"
+        :alt="product.title"
       />
       <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <small> <strong>Adet : </strong> 1 </small>
+        <h5 class="card-title">{{ product.title }}</h5>
+        <small> <strong>Adet : </strong> {{ product.count }} </small>
         <br />
-        <small> <strong>Fiyat : </strong> 10 </small>
+        <small> <strong>Fiyat : </strong> {{ product.price }} </small>
         <br />
-        <small> <strong>Tutar : </strong> 10 </small>
+        <small> <strong>Tutar : </strong> {{ product.totalPrice }} </small>
       </div>
     </appProduct>
   </div>
