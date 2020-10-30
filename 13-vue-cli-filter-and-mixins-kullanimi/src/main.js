0bf094316ec60a6577c30790b2ebf804d9a548fc
Mustafa Pıçakçı <mustafapicakci25@gmail.com>
2020-10-30 23:50:19 +0300

    global mixin olusturma (tavsiye edilmez)

--- 13-vue-cli-filter-and-mixins-kullanimi/src/main.js
+++ 13-vue-cli-filter-and-mixins-kullanimi/src/main.js
@@ -4,6 +4,14 @@ import App from "./App.vue";
 Vue.filter("toLowerCase", (value) => {
   return value.toLowerCase();
 });
+
+
+Vue.mixin({
+  created(){
+    console.log("global mixin oluşturuldu");
+  }
+})
+
 new Vue({
   el: "#app",
   render: (h) => h(App),
