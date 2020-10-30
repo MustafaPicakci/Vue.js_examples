2a1dff07704e848a6f04d96f8d52703afc0e47b8
Mustafa Pıçakçı <mustafapicakci25@gmail.com>
2020-10-30 21:33:04 +0300

    directive'e complex veri göndermek

--- 11-vue-cli-directive-kullanimi/src/App.vue
+++ 11-vue-cli-directive-kullanimi/src/App.vue
@@ -11,7 +11,13 @@
     <div class="row">
       <div class="col-md-6 col-md-offset-3">
         <h3>Custom Directive</h3>
-        <p v-color:background.delay.flash="'lightblue'">
+        <p
+          v-color:background.delay.flash="{
+            mainColor: 'green',
+            secondColor: 'blue',
+            delay: 500,
+          }"
+        >
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores
           aspernatur beatae culpa doloribus, ex facilis fugiat, fugit magni
           molestiae mollitia, nesciunt nihil perspiciatis quaerat quam quas
@@ -40,8 +46,8 @@ export default {
         }
 
         if (binding.modifiers["flash"]) {
-          let firsColor = binding.value;
-          let secondColor = "#fbbd08";
+          let firsColor = binding.value.mainColor;
+          let secondColor = binding.value.secondColor;
           let currentColor = firsColor;
 
           setTimeout(() => {
@@ -55,7 +61,7 @@ export default {
               } else {
                 el.style.color = currentColor;
               }
-            }, 1000);
+            }, binding.value.delay);
           }, delay);
         } else {
           setTimeout(() => {
