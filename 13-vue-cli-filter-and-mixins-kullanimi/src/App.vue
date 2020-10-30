76f312b55fd4a496036a4736a7a4f6c5fe4b165b
Mustafa Pıçakçı <mustafapicakci25@gmail.com>
2020-10-30 23:15:06 +0300

    local filter olusturmak

---
+++ 13-vue-cli-filter-and-mixins-kullanimi/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6 col-md-offset-3">
+        <h3>Filter ve Mixins</h3>
+        <p>{{ message | toUpperCase}}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "Selam Bu bir deneme | bunuduzenle.com",
+    };
+  },
+  filters: {
+    toUpperCase(value) {
+      return value.toUpperCase(); // bu javascripteki fonksiyon...
+    },
+  },
+};
+</script>
+
+<style></style>
