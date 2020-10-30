e48b8cf0ddeb5dfa58398e4c400fb5285865d785
Mustafa Pıçakçı <mustafapicakci25@gmail.com>
2020-10-30 23:18:38 +0300

    global filter tanımlama - birden fazla filter'i zincirleme

--- 13-vue-cli-filter-and-mixins-kullanimi/src/App.vue
+++ 13-vue-cli-filter-and-mixins-kullanimi/src/App.vue
@@ -3,7 +3,7 @@
     <div class="row">
       <div class="col-md-6 col-md-offset-3">
         <h3>Filter ve Mixins</h3>
-        <p>{{ message | toUpperCase}}</p>
+        <p>{{ message | toUpperCase | toLowerCase }}</p>
       </div>
     </div>
   </div>
--- 13-vue-cli-filter-and-mixins-kullanimi/src/main.js
+++ 13-vue-cli-filter-and-mixins-kullanimi/src/main.js
@@ -1,7 +1,10 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 
+Vue.filter("toLowerCase", (value) => {
+  return value.toLowerCase();
+});
 new Vue({
-  el: '#app',
-  render: h => h(App)
-})
+  el: "#app",
+  render: (h) => h(App),
+});
