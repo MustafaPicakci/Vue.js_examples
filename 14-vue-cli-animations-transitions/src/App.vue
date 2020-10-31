315328adff769fe9c362ae250f64fcb70c733aba
Mustafa Pıçakçı <mustafapicakci25@gmail.com>
2020-10-31 21:32:04 +0300

    css transition ile face transition olusturmak

---
+++ 14-vue-cli-animations-transitions/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6 col-md-offset-3">
+        <h3>Animation ve Transition</h3>
+        <hr />
+
+        <button class="btn btn-primary" @click="show = !show">
+          kutuyu göster/gizle
+        </button>
+        <br /><br />
+        <transition name="fade">
+          <div class="alert alert-success" v-if="show">
+            bu bir alert kutusudur
+          </div>
+        </transition>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      show: false,
+    };
+  },
+};
+</script>
+
+<style>
+.fade-enter {
+  opacity: 0;
+}
+.fade-enter-active {
+  transition: opacity 1s;
+}
+
+.fade-leave {
+}
+.fade-leave-active {
+  transition: opacity 1s;
+  opacity: 0;
+}
+</style>
