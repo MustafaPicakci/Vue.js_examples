53f11a15b6e21b8f8e5bebc72f79611e5ddad353
Mustafa Pıçakçı <mustafapicakci25@gmail.com>
2020-11-01 23:00:10 +0300

    Hatalı cevap için component tasarımı

--- 15-hafiza-oyunu/src/components/Failure.vue
+++ 15-hafiza-oyunu/src/components/Failure.vue
@@ -1,7 +1,48 @@
 <template>
-  <h3>Failure</h3>
+  <div class="container">
+    <h1>malesef bilemediniz!!!</h1>
+    <button @click="newGame()">Yeni oyun</button>
+  </div>
 </template>
 <script>
-export default {};
+export default {
+  methods: {
+    newGame() {},
+  },
+};
 </script>
-<style></style>
+<style scoped>
+.container {
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: darkred;
+}
+.container h1 {
+  text-align: center;
+  font-size: 333%;
+  color: white;
+}
+.container button {
+  cursor: pointer;
+  width: 200px;
+  background-color: #fff;
+  color: darkred;
+  border: 1px solid brown;
+  height: 50px;
+  font-size: 15px;
+  border-radius: 5px;
+  outline: none;
+  transition: all 0.5s;
+}
+.container button:hover {
+  background-color: lightgreen;
+  color: #fff;
+}
+</style>
