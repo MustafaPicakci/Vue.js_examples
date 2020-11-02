1efe93cb80cacb9085cdaa9323dd424d946727ea
Mustafa Pıçakçı <mustafapicakci25@gmail.com>
2020-11-02 11:36:41 +0300

    GET İsteği Gönderme ve Verinin Değiştirilmesi

--- 16-vue-cli-HTTP-ile-Sunucuya-Bağlanmak-vue-resource/src/App.vue
+++ 16-vue-cli-HTTP-ile-Sunucuya-Bağlanmak-vue-resource/src/App.vue
@@ -7,6 +7,15 @@
           <input type="text" class="form-control" v-model="userName" />
         </div>
         <button class="btn btn-primary" @click="saveUser()">kaydet</button>
+        <button class="btn btn-success" @click="getUsers()">
+          verileri getir
+        </button>
+        <hr />
+        <ul class="list-group">
+          <li class="list-group-item" v-for="user in userList" :key="user">
+            {{ user.userName }}
+          </li>
+        </ul>
       </div>
     </div>
   </div>
@@ -17,6 +26,7 @@ export default {
   data() {
     return {
       userName: "null",
+      userList: [],
     };
   },
   methods: {
@@ -27,6 +37,17 @@ export default {
         })
         .then((response) => {
           console.log(response);
+        });
+    },
+    getUsers() {
+      this.$http
+        .get("https://vuejs-vue-resource-a01ae.firebaseio.com/users.json")
+        .then((response) => {
+          let data = response.data;
+
+          for (let key in data) {
+            this.userList.push(data[key]);
+          }
         });
     },
   },
