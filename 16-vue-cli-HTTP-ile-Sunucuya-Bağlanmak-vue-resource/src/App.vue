e8381a5260f85c2c64b4ff5ce7f7c7467d765aba
Mustafa Pıçakçı <mustafapicakci25@gmail.com>
2020-11-02 11:28:14 +0300

    POST İsteği Gönderme İşlemi

---
+++ 16-vue-cli-HTTP-ile-Sunucuya-Bağlanmak-vue-resource/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6 col-md-offset-3">
+        <h3>Vue-Resource</h3>
+        <div class="form-froup">
+          <input type="text" class="form-control" v-model="userName" />
+        </div>
+        <button class="btn btn-primary" @click="saveUser()">kaydet</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      userName: "null",
+    };
+  },
+  methods: {
+    saveUser() {
+      this.$http
+        .post("https://vuejs-vue-resource-a01ae.firebaseio.com/users.json", {
+          userName: this.userName,
+        })
+        .then((response) => {
+          console.log(response);
+        });
+    },
+  },
+};
+</script>
+
+<style></style>
