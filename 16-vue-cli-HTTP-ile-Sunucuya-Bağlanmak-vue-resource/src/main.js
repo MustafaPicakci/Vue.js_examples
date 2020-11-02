189b61580c88f744d876337b0633f018a9217969
Mustafa Pıçakçı <mustafapicakci25@gmail.com>
2020-11-02 13:24:51 +0300

    Request için interceptor kullanımı

--- 16-vue-cli-HTTP-ile-Sunucuya-Bağlanmak-vue-resource/src/main.js
+++ 16-vue-cli-HTTP-ile-Sunucuya-Bağlanmak-vue-resource/src/main.js
@@ -3,8 +3,14 @@ import App from "./App.vue";
 import VueResource from "vue-resource";
 
 Vue.use(VueResource);
-Vue.http.options.root ="https://vuejs-vue-resource-a01ae.firebaseio.com/users.json";
-
+Vue.http.options.root =
+  "https://vuejs-vue-resource-a01ae.firebaseio.com/users.json";
+Vue.http.interceptors.push((request, next) => {
+  /*if (request.method == "POST") {
+    request.method = "PUT";
+  }
+  next();*/
+});
 new Vue({
   el: "#app",
   render: (h) => h(App),
