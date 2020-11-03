21ac246d7251c856534fc662729d969db9301a2f
Mustafa Pıçakçı <mustafapicakci25@gmail.com>
2020-11-03 22:39:44 +0300

    beforeLeave Guard Kullanımı

--- 17-vue-cli-vue-router/src/components/user/UserEdit.vue
+++ 17-vue-cli-vue-router/src/components/user/UserEdit.vue
@@ -4,10 +4,28 @@
     <hr />
     <p>ad : {{ $route.query.name }}</p>
     <p>soyad : {{ $route.query.lastName }}</p>
+    <button @click="saved = true" class="btn btn-primary">Onayla</button>
     <div style="height:800px"></div>
     <p id="data">bazı bilgiler...</p>
   </div>
 </template>
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      saved: false,
+    };
+  },
+  beforeRouteLeave(to, from, next) {
+    if (this.saved) {
+      next();
+    } else {
+      if (confirm("tüm bilgileriniz kaybolacak, emin misiniz?")) {
+        next(); //componentten ayrılabilecek
+      } else {
+        next(false); //componentten ayrılamayacak
+      }
+    }
+  },
+};
 </script>
