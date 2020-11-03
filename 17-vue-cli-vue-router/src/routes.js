f2772ccc301a4e215ea6a622b7c56b42f9d3dd78
Mustafa Pıçakçı <mustafapicakci25@gmail.com>
2020-11-03 22:54:50 +0300

    Route’ ları Lazy Load olarak Yüklemek

--- 17-vue-cli-vue-router/src/routes.js
+++ 17-vue-cli-vue-router/src/routes.js
@@ -1,10 +1,47 @@
 import Home from "./components/Home";
+import Header from "./components/Header";
+
+const User = (resolve) => {
+  require.ensure(
+    ["./components/user/User.vue"],
+    () => {
+      resolve(require("./components/user/User.vue"));
+    },
+    "user"
+  ); //buradaki "user" parametresi sayesinde komponentleri grupladık ve bu gruptaki komponentlerden bir tanesi yüklenirse tamamı yüklensin demiş olduk. (Bu parametreyi kullanmak zorunda değiliz.)
+};
+const UserStart = (resolve) => {
+  require.ensure(
+    ["./components/user/UserStart.vue"],
+    () => {
+      resolve(require("./components/user/UserStart.vue"));
+    },
+    "user"
+  ); //buradaki "user" parametresi sayesinde komponentleri grupladık ve bu gruptaki komponentlerden bir tanesi yüklenirse tamamı yüklensin demiş olduk. (Bu parametreyi kullanmak zorunda değiliz.)
+};
+const UserDetail = (resolve) => {
+  require.ensure(
+    ["./components/user/UserDetail.vue"],
+    () => {
+      resolve(require("./components/user/UserDetail.vue"));
+    },
+    "user"
+  ); //buradaki "user" parametresi sayesinde komponentleri grupladık ve bu gruptaki komponentlerden bir tanesi yüklenirse tamamı yüklensin demiş olduk. (Bu parametreyi kullanmak zorunda değiliz.)
+};
+const UserEdit = (resolve) => {
+  require.ensure(["./components/user/UserEdit.vue"], () => {
+    resolve(require("./components/user/UserEdit.vue"));
+  }); //burada user parametresini kullanmadık. Bu komponent sadeec ihtiyaç duyulduğunda yüklenecek
+};
+
+/*
+Aşağıdaki componentler kullanılacağı zaman yükleniyor. Bu işlem yukarıda tanımlandı.
+
 import User from "./components/user/User";
 import UserStart from "./components/user/UserStart";
 import UserEdit from "./components/user/UserEdit";
 import UserDetail from "./components/user/UserDetail";
-import Header from "./components/Header";
-
+*/
 export const routes = [
   {
     path: "/",
