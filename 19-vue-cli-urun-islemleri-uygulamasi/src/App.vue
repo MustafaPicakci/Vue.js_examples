c4c0b500d07fd985899bf4711792858273707f30
Mustafa Pıçakçı <mustafapicakci25@gmail.com>
2020-11-05 22:34:40 +0300

    Component Yapısının Oluşturulması

---
+++ 19-vue-cli-urun-islemleri-uygulamasi/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <app-header></app-header>
+    <app-container></app-container>
+    <app-footer></app-footer>
+  </div>
+</template>
+<script>
+import Header from "./components/shared/Header";
+import Footer from "./components/shared/Footer";
+import ProductPurchase from "./components/products/ProductPurchase";
+import ProductSell from "./components/products/ProductSell";
+import ProductList from "./components/products/ProductList";
+
+export default {
+  components: {
+    appHeader: Header,
+    appFooter: Footer,
+    appContainer: ProductList,
+
+  },
+};
+</script>
+<style></style>
---
+++ 19-vue-cli-urun-islemleri-uygulamasi/src/components/shared/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+  <nav
+    class="navbar fixed-bottom  navbar-light"
+    style="background-color: #e3f2fd;"
+  >
+    <span class="navbar-text mr-auto">
+      <strong>vuejsegitim.com</strong> | gkandemir
+    </span>
+    <span class="navbar-text navbar-nav my-2 my-lg-0 mr-3">
+      <strong>Alış Tutarı : </strong>
+      <span class="badge badge-danger">34,56</span>
+    </span>
+    <span class="navbar-text navbar-nav my-2 my-lg-0 mr-3">
+      <strong>Satış Tutarı : </strong>
+      <span class="badge badge-success">34,56</span>
+    </span>
+    <span class="navbar-text navbar-nav my-2 my-lg-0">
+      <strong>Bakiye : </strong>
+      <span class="badge badge-primary">34,56</span>
+    </span>
+  </nav>
+</template>
