845c58cd01609f1dc9be1be5881b3bdf1990e5b1
Mustafa Pıçakçı <mustafapicakci25@gmail.com>
2020-11-05 23:48:58 +0300

    Vue Resource' un projeye dahil edilmesi

--- 19-vue-cli-urun-islemleri-uygulamasi/src/main.js
+++ 19-vue-cli-urun-islemleri-uygulamasi/src/main.js
@@ -2,6 +2,10 @@ import Vue from "vue";
 import App from "./App.vue";
 import { router } from "./router";
 import { store } from "./store/store";
+import VueResource from "vue-resource";
+
+Vue.use(VueResource);
+
 new Vue({
   el: "#app",
   render: (h) => h(App),
