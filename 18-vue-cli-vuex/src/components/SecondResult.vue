7ab7118b3b56cbb47d7ca5cc4fd2901112f09cb5
Mustafa Pıçakçı <mustafapicakci25@gmail.com>
2020-11-05 16:01:48 +0300

    mapGetters Hook Fonksiyonlarının Kullanımı

--- 18-vue-cli-vuex/src/components/SecondResult.vue
+++ 18-vue-cli-vuex/src/components/SecondResult.vue
@@ -8,12 +8,25 @@
 import { mapGetters } from "vuex";
 export default {
   // props: ["counter"],
-  computed: mapGetters(["getDoubleCounter", "stringCounter"]), //computed içindeki değişken isimlerini farklı isimlerle değiştirip kullanmak istersek
+ // computed: mapGetters(["getDoubleCounter", "stringCounter"]), //computed içindeki değişken isimlerini farklı isimlerle değiştirip kullanmak istersek
   //mapGetters'ın dizi değil obje parameter almasını sağlıyoruz.(aşağıdaki gibi)
   /*computed: mapGetters({
     double: "getDoubleCounter",
     stringC: "stringCounter",
   }),*/
+
+  //eğer mapgetters ile eriştiğimiz storedaki getterslar dışında, bu componentte computed properties kullanmak istersek ikisin anyı anda
+  //kullanabilmek için '...mapgetters'şeklinde kullanmalıyız. Bu , mapgetters ile getterdaki fonksiyonları al ve buraya kopyala anlamına geliyor
+  // ve biz de devamına kendi computed propertyleri ekleyebiliyoruz.(örnek kullanım aşağıdadır)
+  computed: {
+    ...mapGetters({
+      double: "getDoubleCounter",
+      stringC: "stringCounter",
+    }),
+    custemProp(){
+      //kendi comp. prop. de ekleyebildik
+    }
+  },
 };
 </script>
 <style scoped>
