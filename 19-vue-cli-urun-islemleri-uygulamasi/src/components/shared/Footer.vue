3a45b6812835f6cfe9848c09d4b82005d1a44344
Mustafa Pıçakçı <mustafapicakci25@gmail.com>
2020-11-06 23:16:03 +0300

    Footer Üzerindeki Verilerin Güncellenmesi

--- 19-vue-cli-urun-islemleri-uygulamasi/src/components/shared/Footer.vue
+++ 19-vue-cli-urun-islemleri-uygulamasi/src/components/shared/Footer.vue
@@ -8,15 +8,22 @@
     </span>
     <span class="navbar-text navbar-nav my-2 my-lg-0 mr-3">
       <strong>Alış Tutarı : </strong>
-      <span class="badge badge-danger">34,56</span>
+      <span class="badge badge-danger">{{getTradeResult.purchase}}</span>
     </span>
     <span class="navbar-text navbar-nav my-2 my-lg-0 mr-3">
       <strong>Satış Tutarı : </strong>
-      <span class="badge badge-success">34,56</span>
+      <span class="badge badge-success">{{getTradeResult.sale}}</span>
     </span>
     <span class="navbar-text navbar-nav my-2 my-lg-0">
       <strong>Bakiye : </strong>
-      <span class="badge badge-primary">34,56</span>
+      <span class="badge badge-primary">{{getTradeResult.balance}}</span>
     </span>
   </nav>
 </template>
+
+<script>
+import {mapGetters} from "vuex";
+export default {
+  computed: mapGetters(["getTradeResult"])
+};
+</script>
