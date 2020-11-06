b8740db6a12b2190816a7ace26112cee4a0627b5
Mustafa Pıçakçı <mustafapicakci25@gmail.com>
2020-11-06 23:34:04 +0300

    Kaydet Butonunun verilere göre aktif/pasif olmasını sağlamak

--- 19-vue-cli-urun-islemleri-uygulamasi/src/components/products/ProductPurchase.vue
+++ 19-vue-cli-urun-islemleri-uygulamasi/src/components/products/ProductPurchase.vue
@@ -43,7 +43,13 @@
             ></textarea>
           </div>
           <hr />
-          <button @click="saveProduct" class="btn btn-primary">Kaydet</button>
+          <button
+            @click="saveProduct"
+            :disabled="saveEnabled"
+            class="btn btn-primary"
+          >
+            Kaydet
+          </button>
         </div>
       </div>
     </div>
@@ -63,7 +69,21 @@ export default {
   },
   methods: {
     saveProduct() {
-      this.$store.dispatch("saveProduct",this.product)
+      this.$store.dispatch("saveProduct", this.product);
+    },
+  },
+  computed: {
+    saveEnabled() {
+      if (
+        this.product.title.length > 0 &&
+        this.product.count > 0 &&
+        this.product.price > 0 &&
+        this.product.description.length > 0
+      ) {
+        return false;
+      } else {
+        return true;
+      }
     },
   },
 };
