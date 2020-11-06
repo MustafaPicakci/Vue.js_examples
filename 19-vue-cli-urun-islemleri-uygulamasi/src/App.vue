f8620dba0fa52c20b7c744992ea111d8e3d60b27
Mustafa Pıçakçı <mustafapicakci25@gmail.com>
2020-11-06 23:54:29 +0300

    Router geçişlerine Animasyon Eklemek

--- 19-vue-cli-urun-islemleri-uygulamasi/src/App.vue
+++ 19-vue-cli-urun-islemleri-uygulamasi/src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <app-header></app-header>
+    <transition name="fade" mode="out-in">
     <router-view></router-view>
+    </transition>
     <app-footer></app-footer>
   </div>
 </template>
@@ -22,4 +24,16 @@ export default {
   },
 };
 </script>
-<style></style>
+<style>
+.fade-enter{
+  opacity: 0;
+}
+.fade-enter-active{
+  transition: opacity .3s ease-out;
+}
+.fade-leave{}
+.fade-leave-active{
+  transition: opacity .3s ease-out;
+  opacity: 0;
+}
+</style>
