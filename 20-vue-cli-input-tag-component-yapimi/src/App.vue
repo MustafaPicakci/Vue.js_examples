95b1dde5fb3547eb5416bb8b345eeb294e09d4e5
Mustafa Pıçakçı <mustafapicakci25@gmail.com>
2020-11-09 00:33:32 +0300

    Ana yapının oluşturulması

---
+++ 20-vue-cli-input-tag-component-yapimi/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="tag-container">
+    <span class="tag">
+      <span class="content">Vuejs</span>
+      <span class="close">X</span>
+    </span>
+    <span class="tag">
+      <span class="content">Vuejs</span>
+      <span class="close">X</span>
+    </span>
+    <span class="tag">
+      <span class="content">Vuejs</span>
+      <span class="close">X</span>
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style>
+body {
+  font-family: sans-serif;
+}
+.tag-container {
+  border: 1px solid #ccc;
+  padding: 20px;
+}
+.tag {
+  background-color: #fbbd08;
+  padding: 10px;
+  color: #000;
+  cursor: default;
+  font-size: 14px;
+}
+.tag .close{
+  font-size: 12px;
+  cursor: pointer;
+}
+</style>
