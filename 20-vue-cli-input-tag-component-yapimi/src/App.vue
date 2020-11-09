fd262bed12a260b1925b5e8187393955b81e1849
Mustafa Pıçakçı <mustafapicakci25@gmail.com>
2020-11-09 12:06:10 +0300

    Etiket eklerken verinin kontrol edilmesi ve Hata mesajlarının gösterimi

--- 20-vue-cli-input-tag-component-yapimi/src/App.vue
+++ 20-vue-cli-input-tag-component-yapimi/src/App.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="tag-container">
     <span class="tag" v-for="tag in tags" :key="tag">
-      <span class="content">{{tag}}</span>
+      <span class="content">{{ tag }}</span>
       <span class="close">X</span>
     </span>
 
-    <input type="text" @keydown.enter="addTag">
+    <input type="text" @keydown.enter="addTag" />
+    <div v-if="error" class="error">Bu etiket daha önceden eklenmiş!!</div>
   </div>
 </template>
 
@@ -13,16 +14,34 @@
 export default {
   data() {
     return {
-      tags:['deneme','test']
-    }
+      tags: ["deneme", "test"],
+      error: false,
+    };
   },
-methods:{
-  addTag(event){
-    this.tags.push(event.target.value)
-    event.target.value='';
-  }
-}
+  methods: {
+    addTag(event) {
+      let text = event.target.value;
+      let matchedTag = false;
+      if (text.length > 0) {
+        this.tags.forEach((tag) => {
+          if (tag.toLowerCase() === text.toLowerCase()) {
+            matchedTag = true;
+          }
+        });
 
+        if (!matchedTag) {
+          this.tags.push(text);
+          text = "";
+        } else {
+          this.error = true;
+
+          setTimeout(() => {
+            this.error = false;
+          }, 2000);
+        }
+      }
+    },
+  },
 };
 </script>
 
@@ -42,13 +61,18 @@ body {
   font-size: 14px;
   margin-right: 10px;
 }
-.tag .close{
+.tag .close {
   font-size: 12px;
   cursor: pointer;
 }
-input{
+input {
   outline: none;
   height: 30px;
   width: 100px;
+}
+.error {
+  font-size: 12px;
+  color: red;
+  margin: 5px;
 }
 </style>
